5bb40ef2335cef3a37a33b54f9ecff421a6202b8
Matty Smith <matthewcsmith18@gmail.com>
2021-02-20 21:12:07 -0500

    g

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const ConvertLib = artifacts.require('ConvertLib');
+const MetaCoin = artifacts.require('MetaCoin');
 
-const HelloWorld = artifacts.require("HelloWorld");
-const initMessage = "Hello world!"
+const HelloWorld = artifacts.require('HelloWorld');
+const initMessage = 'Hello world!';
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   //deployer.deploy(ConvertLib);
   //deployer.link(ConvertLib, MetaCoin);
   //deployer.deploy(MetaCoin);
